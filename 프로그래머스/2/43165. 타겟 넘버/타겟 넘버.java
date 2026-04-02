419501cd00ea78f3a6f3153e03f7611aba076523
hyosungan <104579283+hyosungan@users.noreply.github.com>
2026-04-02 16:28:05 +0900

    [level 2] Title: 타겟 넘버, Time: 4.38 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    int answer=0;
+    public int solution(int[] numbers, int target) {
+        dfs(0,numbers,target,0);
+        
+        return answer;
+    }
+    public void dfs(int idx,int[] numbers,int target,int sum){
+        if (idx==numbers.length){
+            if(target==sum){
+                answer+=1;
+                
+            }
+            return;
+        }
+        dfs(idx+1,numbers,target,sum+numbers[idx]);
+        dfs(idx+1,numbers,target,sum-numbers[idx]);
+    }
+    
+}
